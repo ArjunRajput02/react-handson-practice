b5abc41ad1152366446dc54f9cefd060913c0609
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-01-29 17:08:40 +0530

    fix: format code

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { CourseGoal } from './component/CourseGoal'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { CourseGoal } from "./component/CourseGoal";
 
 function App() {
   return (
     <div id="app" data-testid="app">
       <h1>Time to Practice</h1>
       <p>One course, many goals! 🎯</p>
-      
+
       <ul>
-      {/* Rendering Props here */}
-      <CourseGoal
-      title = "Learn React"
-      description = "In-depth"
-      />
-      <CourseGoal 
-       title = "Practice Working"
-      description = "Practice Working With Deep "
-      />
-       
+        {/* Rendering Props here */}
+        <CourseGoal title="Learn React" description="In-depth" />
+        <CourseGoal
+          title="Practice Working"
+          description="Practice Working With Deep "
+        />
       </ul>
     </div>
   );
