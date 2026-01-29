2e0760e88f3781ac950f66fd6b40242b82cb6138
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-01-29 10:31:02 +0530

    feat: add props in react

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { CourseGoal } from './component/CourseGoal'
+
+
+function App() {
+  return (
+    <div id="app" data-testid="app">
+      <h1>Time to Practice</h1>
+      <p>One course, many goals! 🎯</p>
+      
+      <ul>
+      {/* Rendering Props here */}
+      <CourseGoal
+      title = "Learn React"
+      description = "In-depth"
+      />
+      <CourseGoal 
+       title = "Learn State"
+      description = "In-depth"
+      />
+       
+      </ul>
+    </div>
+  );
+}
+
+export default App;
