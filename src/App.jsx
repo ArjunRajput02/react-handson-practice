af63ad5e4b04b3b24d2c692e405f5cb594187480
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-02-01 18:18:21 +0530

    feat: add forward props

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AddUseState from "./component/addUseState/AddUseState.jsx";
 import AddConditionalState from "./component/addConditionalState/AddConditionalState.jsx";
 import AddDynamicStyle from "./component/addDynamicStyle/AddDynamicStyle.jsx";
 import AddListDynamic from "./component/addListDynamic/AddListDynamic.jsx";
+import AddProps from "./component/addForwardProps/AddProps.jsx";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <AddConditionalState />
       <AddDynamicStyle />
       <AddListDynamic />
+      <AddProps />
     </>
   );
 }
