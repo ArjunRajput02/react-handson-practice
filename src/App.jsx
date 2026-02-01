b40847d9454df382b7c86691513230fa3d4fbbb9
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-02-01 17:02:33 +0530

    feat: add fragment in react

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import AddUseState from "./component/addUseState/AddUseState.jsx";
 import AddConditionalState from "./component/addConditionalState/AddConditionalState.jsx";
 import AddDynamicStyle from "./component/addDynamicStyle/AddDynamicStyle.jsx";
 import AddListDynamic from "./component/addListDynamic/AddListDynamic.jsx";
+import SummaryText from "./component/addFragment/SummmaryText.jsx";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       <AddConditionalState />
       <AddDynamicStyle />
       <AddListDynamic />
+      <SummaryText />
     </>
   );
 }
---
+++ src/component/addFragment/AddFragment..jsx
@@ -0,0 +1,8 @@
+export default function AddFragment({ text }) {
+  return (
+    <>
+      <h1 id="summary-h1">Summary</h1>
+      <p>{text}</p>
+    </>
+  );
+}
