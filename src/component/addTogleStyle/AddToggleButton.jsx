bf9b33a6d3967c129aa5b4732b54c1dceaa5c430
Arjun Rajput <arjun.rajput@seaflux.tech>
2026-02-03 18:38:52 +0530

    feat: create toggle button

---
+++ src/component/addTogleStyle/AddToggleButton.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  const [Color, setColor] = React.useState(false);
+  function handelClick() {
+    if (Color === false) {
+      setColor(true);
+    } else {
+      setColor(false);
+    }
+  }
+  return (
+    <div id="togglebutton">
+      <p style={{ color: Color ? "red" : "white" }}>Style me!</p>
+      <button onClick={handelClick}>Toggle style</button>
+    </div>
+  );
+}
